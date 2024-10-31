54db2ca34a634decc4928fbc23d47b97d18e37b9
Prokop <prokop.schield@protonmail.com>
2024-10-31 12:23:17 +0100

    chore: format

--- src/cli.ts
+++ src/cli.ts
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 
-import nsblob from '.';
+import nsblob from ".";
 
 async function print() {
-	for (const arg of process.argv.slice(2)) {
-		process.stdout.write(await nsblob.fetch(arg));
-	}
+    for (const arg of process.argv.slice(2)) {
+        process.stdout.write(await nsblob.fetch(arg));
+    }
 }
 
 const print_promise = print();
 
 const parts = new Array<Buffer>();
 
-process.stdin.on('data', (chunk) => parts.push(chunk));
-process.stdin.on('end', async () => {
-	const hash = await nsblob.store(Buffer.concat(parts));
+process.stdin.on("data", (chunk) => parts.push(chunk));
+process.stdin.on("end", async () => {
+    const hash = await nsblob.store(Buffer.concat(parts));
 
-	await print_promise;
+    await print_promise;
 
-	if (parts.length) {
-		console.log(hash);
-	}
+    if (parts.length) {
+        console.log(hash);
+    }
 
-	nsblob.socket.close();
+    nsblob.socket.close();
 });
